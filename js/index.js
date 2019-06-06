def10932054397770a31ae98d771e35995dda350
hex <1057775855@qq.com>
2019-06-06 22:08:44 +0800

    login-and-home

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 var vm = new Vue({
-    el: "#app"
+    el: "#app",
+    data: {
+        src: "./sound.html"
+    },
+    methods: {
+        handleClick (e) {
+            console.log(e.target)
+            let target = $(e.target);
+            let src = target.attr("to");
+            if (src !== undefined && src !== null && src !== "") {
+                this.src = src;
+            }
+        }
+    }
 })
